ebf12e84cd34f739216f8654cc262837dd08020c
shaminR <shamin99@outlook.com>
2018-11-04 16:43:08 -0700

    Update lab8clock.cpp

--- lab8clock.cpp
+++ lab8clock.cpp
@@ -1,50 +1,83 @@
 //lab8clock.cpp
 #include "lab8clock.h"
-Clock(){
+Clock::Clock(){
 	hour = 0;
 	minute = 0;
 	second = 0;
 }
-Clock(int m){
+Clock::Clock(int m){
 	if(m<0){
 		hour = 0;
 		minute = 0;
 		second = 0;
-	}
-	sec_to_hms(m);
+	}else
+		sec_to_hms(m);
 }
-Clock(int s, int m, int h){
-	if((s>59 || s<0) || (m>59 || s<m) || (h>23 || h<0)){
+Clock::Clock(int h, int m, int s){
+	if((s>59 || s<0) || (m>59 || m<0) || (h>23 || h<0)){
 		hour = 0;
 		minute = 0;
 		second = 0;
+	}else{
+		second = s;
+		minute = m;
+		hour = h;
 	}
-	second = s;
-	minute = m;
-	hour = h;
 }
-int hms_to_sec(){
+int Clock::hms_to_sec(){
 	return second + (minute*60) + (hour*3600);
 }
-void sec_to_hms(int n){
-	int sum = n;
+void Clock::sec_to_hms(int n){
 	hour = n/3600;
-	minute = 
+	minute = (n%3600)/60;
+	second = (n%3600)%60;
+	if(hour>=24){
+		hour = hour%24;
+	}
+	if(hour == 0 && minute == 0 && second<0){
+		hour = 23;
+		minute = 59;
+		second = 60 + second;
+	}
 }
-int get_hour()const{
+int Clock::get_hour()const{
 	return hour;
 }
-int get_minute()const{
+int Clock::get_minute()const{
 	return minute;
 }
-int get_second()const[
+int Clock::get_second()const{
 	return second;
 }
-void set_hour(int h){
-	
+void Clock::set_hour(int h){
+	if(h>=0 && h<=23){
+		hour = h;
+	}
 }
-void set_minute(int m);
-void set_second(int s);
-void increment();
-void decrement();
-void add_seconds(int s);
+void Clock::set_minute(int m){
+	if(m>=0 && m<=59){
+		minute = m;
+	}
+}
+void Clock::set_second(int s){
+	if(s>=0 && s<=59){
+		second = s;
+	}
+}
+void Clock::increment(){
+	int seconds = hms_to_sec();
+	seconds++;
+	sec_to_hms(seconds);
+}
+void Clock::decrement(){
+	int seconds = hms_to_sec();
+	seconds--;
+	sec_to_hms(seconds);
+}
+void Clock::add_seconds(int s){
+	if(s>-1){
+		int seconds = hms_to_sec();
+		seconds+=s;
+		sec_to_hms(seconds);
+	}
+}
